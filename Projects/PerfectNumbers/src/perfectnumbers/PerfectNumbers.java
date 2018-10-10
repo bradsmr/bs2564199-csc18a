265bd09ffe0d344ff57767845cb2e284dc215d9e
Bradley Summers <bradsmr@gmail.com>
2018-10-10 15:17:59 -0700

    started hw4 perfect numbers

---
+++ Projects/PerfectNumbers/src/perfectnumbers/PerfectNumbers.java
@@ -0,0 +1,28 @@
+// a program to evaluate and display perfect numbers
+// author: Bradley Summers
+// date: 10/09/2018
+
+package perfectnumbers;
+import java.util.Scanner;
+
+public class PerfectNumbers {
+
+    public static void main(String[] args) {
+        
+        // declare variables
+        Scanner input = new Scanner(System.in); // scanner to get input
+        
+        int largest_number;
+        
+        // display instructions & get input
+        System.out.print("Enter the largest integer to try to display: ");
+        largest_number = input.nextInt();
+        
+        
+    }
+
+    public String isPerfect(int n) {
+        
+        return null;
+    }
+}
